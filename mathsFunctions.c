c14c615dfa2db67561b1c110a40eb40b635bbb61
d3f3c77 <36715575+d3f3c77@users.noreply.github.com>
2018-11-27 10:06:11 +0000

    Maths function, program simple arithmrtic actions.

---
+++ mathsFunctions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	//Var
+	int a = 4;
+	int b = 8; 
+	int c = 1;
+	int d = 1;
+	
+	printf("Addition: %d \n", a+b);
+	printf("Subtraction: %d \n", b-a);
+	printf("Multiplication: %d \n", a*b);
+
+	return 0;
+}
